544364c2397f2f15e8178761376b326ffe8598ac
Aditya Chanana <achanana@andrew.cmu.edu>
2020-03-25 12:06:34 -0400

    Added drop downs for QCEW filters

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,6 +10,37 @@ class HomeController < ApplicationController
       @reply = parsed_json
       session[:series_id] = params[:series_id]
     end
+
+    @area_codes = CSV.read('csv_files/area_titles.csv')[1..]
+    @data_types = CSV.read('csv_files/datatype_titles.csv')[1..]
+    @industries = CSV.read('csv_files/industry_titles.csv')[1..]
+    @ownership = CSV.read('csv_files/ownership_titles.csv')[1..]
+    @sizes = CSV.read('csv_files/size_titles.csv')[1..]
+    for area_code in @area_codes do
+      tmp = area_code[0]
+      area_code[0] = area_code[1]
+      area_code[1] = tmp
+    end
+    for data_type in @data_types do
+      tmp = data_type[0]
+      data_type[0] = data_type[1]
+      data_type[1] = tmp
+    end
+    for industry in @industries do
+      tmp = industry[0]
+      industry[0] = industry[1]
+      industry[1] = tmp
+    end
+    for ownership in @ownership do
+      tmp = ownership[0]
+      ownership[0] = ownership[1]
+      ownership[1] = tmp
+    end
+    for size in @sizes do
+      tmp = size[0]
+      size[0] = size[1]
+      size[1] = tmp
+    end
   end
   def download_csv
     @manager = JsonManager.new("https://api.bls.gov/publicAPI/v2/timeseries/data/")
