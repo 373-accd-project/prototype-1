184c9f89a44d200756b8cf517a12cb9d026bbb59
Jessica Li <jli6@andrew.cmu.edu>
2020-04-19 02:23:43 -0400

    NAICS checkboxes works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,3 +10,24 @@
 //       $('#' +$(this).val()).show();
 //   });
 // });
+
+// // Treeview Initialization
+// $(document).ready(function() {
+//     $('.treeview').mdbTreeview();
+//     console.log("hello");
+//   });
+
+
+
+function checkboxNest(){
+    var toggler = document.getElementsByClassName("caret");
+    var i;
+    console.log("hello");
+    
+    for (i = 0; i < toggler.length; i++) {
+      toggler[i].addEventListener("click", function() {
+        this.parentElement.querySelector(".nested").classList.toggle("active");
+        this.classList.toggle("caret-down");
+      });
+    }
+}
