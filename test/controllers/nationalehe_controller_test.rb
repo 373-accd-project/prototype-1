0972917d3a0ba5089561a40a084a680e79d49d92
Aditya Chanana <achanana@andrew.cmu.edu>
2020-04-24 01:05:57 -0400

    Added a controller test

--- test/controllers/nationalehe_controller_test.rb
+++ test/controllers/nationalehe_controller_test.rb
@@ -4,4 +4,9 @@ class NationaleheControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "goes to home if not logged in" do
+    get "/nationalehe"
+    assert_redirected_to "/login"
+  end
 end
