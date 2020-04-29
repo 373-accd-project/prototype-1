ce8a901b43e67aea7b74ba3180dd5c3986161bf5
Aditya Chanana <achanana@andrew.cmu.edu>
2020-04-29 03:37:27 -0400

    Added user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should validate_presence_of(:api_key)
+  should validate_presence_of(:username)
+  should validate_presence_of(:password).on(:create)
+  should validate_presence_of(:password_confirmation).on(:create)
+
+  should validate_length_of(:password).is_at_least(4)
+
+  should validate_uniqueness_of(:username).case_insensitive
+
+  should have_secure_password
 end
